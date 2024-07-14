6be42e9e4f1b13b1f6e5bfdea127219705abccd2
Mohan Acharya <you@example.com>
2024-07-14 23:57:53 +0530

    first commit

---
+++ app/src/main/java/com/example/sdcard/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.sdcard;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private final int CAMERA_REQ_CODE = 100;
+    ImageView imgCamera;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgCamera = findViewById(R.id.imgCamera);
+        Button btnCamera = findViewById(R.id.btnCamera);
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK) {
+                    if (result.getResultCode() == CAMERA_REQ_CODE) {
+                        //for camera
+                        Bitmap img = (Bitmap) (result.getData().getExtras().get("data"));
+                        imgCamera.setImageBitmap(img);
+                    }
+                }
+            }
+        });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultLauncher.launch(iCamera);
+            }
+        });
+    }
+}
